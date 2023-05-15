1938d77fdfd2ecda519f3bac3536620595934a0f
Gem Kalu <kalublessingnike@gmail.com>
2023-05-15 06:19:24 -0700

    multiplicatioOfIntergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first interger
+ * @b: Second integer
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	a = 82;
+	b = 122;
+
+	return (a * b);
+}
