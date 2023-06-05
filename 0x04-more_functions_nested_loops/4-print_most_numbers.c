b4e35bd1a9f20b680ba309137a800df0b27e0f07
Gem Kalu <kalublessingnike@gmail.com>
2023-06-05 05:02:26 -0700

    print more numbers and print line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-	i = 0;
-	while (i < 10)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != 2 && i != 4)
-			_putchar(i + '0');
-		i++;
-
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
